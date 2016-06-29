ce33e10fa40fbacd0dddc5563116514024502fb8
Ankit Sharma <vicnwz@gmail.com>
2016-06-29 21:47:07 +0530

    first load

--- app/appModule.js
+++ app/appModule.js
@@ -2,9 +2,6 @@
  * @author Ankit Sharma <vicNWZ@gmail.com>
  */
 
-/*
- * AngularJS Web Application
- */
 var app = angular.module('webProfile', [
   'ngRoute',
   'ui.bootstrap'
@@ -19,28 +16,54 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/", {templateUrl: "partials/home.html", controller: "homeController as homeCtrl"})
     // Pages
     //.when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/spaceShoot", {templateUrl: "partials/externalApps.html", controller: "externalAppController as eaCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html"});
 }]);
 
+app.run(function(){
+  window.addEventListener('resize', function(profileService){
+    profileService.calcMobView();
+  });
+});
+
 app.constant('appConstants', {
 	xBox: 5,
 	yBox: 7,
-	xLow: 50,
-	xUp: 700,
-	yLow: 100,
-	yUp: 500,
-	blkSize: 100,
-	switchingDelay: 400
+	xLow: 10,
+	xUp: 80,
+	yLow: 10,
+	yUp: 80,
+	blkSizeX: 11.5,
+	blkSizeY: 16,
+	switchingDelay: 400,
+  mobWidth: 600
 });
 
-
-/*pp.directive('myDirective', function() {
+app.directive('inputM', function($compile){
   return {
-		scope: {
-			myData: '='
-		},
-		template: '<div>Some Text From Directive and {{myData}}</div>'
-  };
+    restrict: 'E',
+    scope: {
+      mName: '@',
+      mType: '@',
+      mPlaceholder: '@',
+      mModel: '=',
+      mLabel: '@',
+      mRow: '@'
+    },
+    // controllerAs: ',
+     //bindToController: true,
+    controller: function($scope){
+      //var scope = this;
+      $scope.activeC= "";
+      $scope.activated = function(isActive){
+        if(isActive || ($scope.mModel != '' && $scope.mModel != null)){
+          $scope.activeC= "active";
+        }else{
+          $scope.activeC= "";
+        }
+      }
+    },
+    template: '<div class="input-m-class"><label class="{{activeC}}" ng-bind="mLabel"></label><input type="{{mType}}" row="{{mRow}}" name="{{mName}}" placeholder="{{mPlaceholder}}" ng-model="mModel" ng-focus="activated(true)" ng-blur="activated(false)"> </div>'
+  }
 });
-*/
--- app/controllers/navController.js
+++ app/controllers/navController.js
@@ -1,24 +1,49 @@
-app.controller("navController", function($timeout, profileService){
+app.controller("navController", function($rootScope, $timeout, $location, $uibModal, profileService){
 	var ctrl = this;
 	ctrl.links = profileService.navLinks;
 	ctrl.currentNavIndex = null;
 	ctrl.navTitle = profileService.navTitle;
-	
-	
+
+
 	ctrl.redirectTo = function(index){
 		if(ctrl.currentNavIndex == null){
+		profileService.currentNavIndex = index;
 			profileService.boxes.methods.serializeBox();
 			ctrl.currentNavIndex = index;
+			broadcastNavInfo(500);
 		} else if(index != ctrl.currentNavIndex){
-			profileService.boxes.methods.randomizeBox();
+			profileService.currentNavIndex = index;
+			//profileService.boxes.methods.serializeBox();
 			ctrl.currentNavIndex = index;
-			$timeout(function(){
-				profileService.boxes.methods.serializeBox();
-			}, 500);
+			broadcastNavInfo(0);
 		} else{
+			profileService.currentNavIndex = null;
 			profileService.boxes.methods.randomizeBox();
 			ctrl.currentNavIndex = null;
+			broadcastNavInfo(0);
+		}
+
+		if(ctrl.links[index].redirect){
+			profileService.currentApp = index;
+			$location.path('/spaceShoot');
 		}
 	}
-  
+
+	ctrl.resumePreviewer = function(){
+		var modalInstance = $uibModal.open({
+	      animation: true,
+	      template: '<embed src="images/resume.pdf" type="application/pdf">',
+	      size: 'lg'
+	    });
+	}
+
+	var broadcastNavInfo = function(time){
+		if(time == 0){
+			$rootScope.$broadcast('navChanged');
+		}else
+			$timeout(function(){
+				$rootScope.$broadcast('navChanged');
+			}, time);
+	}
+
 });
